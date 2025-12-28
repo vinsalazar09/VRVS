e1e8b55f589e41cace3b3da8665e4285c9dd6253
vinsalazar09 <vinsalazar09@gmail.com>
2025-12-27 21:40:45 -0300

    bump: CACHE_NAME (emergência splash)

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service Worker para VRVS - Funcionamento Offline
 // ATUALIZAR ESTA VERSÃO SEMPRE QUE FIZER MUDANÇAS PARA FORÇAR ATUALIZAÇÃO
-const CACHE_NAME = "vrvs-v5.3.32-fix-ajuste-3-treino-livre-formatacao-20251225-1840";
+const CACHE_NAME = "vrvs-v5.3.33-emergencia-splash-20251227-2305";
 
 // Arquivos essenciais para cache
 const FILES_TO_CACHE = [
