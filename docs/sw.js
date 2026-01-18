d2a18d058de0a94270bb4ddd5ef16cd5ed25a87c
vinsalazar09 <vinsalazar09@gmail.com>
2026-01-18 02:07:56 -0300

    P1-INSTRUMENTACAO: debug visual caret iOS (removível, default OFF)

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service Worker para VRVS - Funcionamento Offline
 // ATUALIZAR ESTA VERSÃO SEMPRE QUE FIZER MUDANÇAS PARA FORÇAR ATUALIZAÇÃO
-const CACHE_NAME = "vrvs-v5.3.99-p2-p3-fix-data-utc-box-exclusao-20260118-0144";
+const CACHE_NAME = "vrvs-v5.3.99-p1-instrumentacao-caret-20260118-0155";
 
 // Arquivos essenciais para cache
 const FILES_TO_CACHE = [
