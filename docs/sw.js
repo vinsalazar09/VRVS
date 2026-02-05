f9a603cc7a2b24e0fa72dc20dd83b972963806bf
vinsalazar09 <vinsalazar09@gmail.com>
2026-02-05 00:31:42 -0300

    Revert "hotfix: toast bulk diario double-raf"
    This reverts commit 9d77257f1c7bbc00f3293d2825004362e5d2bb72.

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 // Service Worker para VRVS - Funcionamento Offline
 // ATUALIZAR ESTA VERSÃO SEMPRE QUE FIZER MUDANÇAS PARA FORÇAR ATUALIZAÇÃO
-const CACHE_NAME = "vrvs-v5.3.195-hotfix-toast-diario-double-raf-20260204-0800";
+const CACHE_NAME = "vrvs-v5.3.194-hotfix-toast-diario-clean-20260204-0700";
 
 // Arquivos essenciais para cache
 const FILES_TO_CACHE = [
